65921db9501b37ee2155eae4ca36f5f73f7f20e8
Shivam Kakkar <37249646+shivamkakkar4@users.noreply.github.com>
2021-03-05 18:05:07 +0530

    Backend Structured

--- server/index.js
+++ server/index.js
@@ -3,11 +3,15 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import postRoutes from "./routes/posts.js";
+
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use("/posts", postRoutes);
 
 const CONNECTION_URL = "mongodb://localhost:27017/memories";
 const PORT = process.env.PORT || 5000;
