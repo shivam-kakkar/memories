d2c855cf6be352fa11a9fa0f466fe93892934c51
Shivam Kakkar <37249646+shivamkakkar4@users.noreply.github.com>
2021-03-06 06:01:16 +0530

    Get and Create Posts Redux Setup

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Post from "./Post/Post";
+import useStyles from "./styles";
 
 const Posts = () => {
+  const classes = useStyles();
   return (
     <div>
       <h1>Posts</h1>
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
 
+import { reducers } from "./reducers";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
