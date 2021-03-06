d6801658a51a626b24418c4c47b7cfaec3bc732d
Shivam Kakkar <37249646+shivamkakkar4@users.noreply.github.com>
2021-03-06 06:31:34 +0530

    Created Form Component and create post functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import { useDispatch } from "react-redux";
 
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
+import { getPosts } from "./actions/posts";
 import useStyles from "./styles";
 import memories from "./images/memories.png";
 
 const App = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
   return (
     <Container maxWidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
