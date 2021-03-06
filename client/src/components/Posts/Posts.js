4889739ffce2b927fc1591394c763778d725a141
Shivam Kakkar <37249646+shivamkakkar4@users.noreply.github.com>
2021-03-06 06:43:59 +0530

    Implemented Get Posts functionality

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Grid, CircularProgress } from "@material-ui/core";
 import { useSelector } from "react-redux";
 
 import Post from "./Post/Post";
@@ -8,14 +9,16 @@ const Posts = () => {
   const posts = useSelector(state => state.posts);
   const classes = useStyles();
 
-  console.log(posts);
-  return (
-    <div>
-      <h1>Posts</h1>
-      <Post />
-      <Post />
-      <Post />
-    </div>
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+      {posts.map(post => (
+        <Grid key={post._id} item xs={12} sm={6} md={6}>
+          <Post post={post} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
