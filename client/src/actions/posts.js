65a358e7e60e6d842577ed437a042b82612bee77
Shivam Kakkar <37249646+shivamkakkar4@users.noreply.github.com>
2021-03-06 08:46:33 +0530

    Like Post Functionality

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,4 +1,4 @@
-import { FETCH_ALL, CREATE, UPDATE, DELETE } from "../constants/actionTypes";
+import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from "../constants/actionTypes";
 
 import * as api from "../api/index.js";
 
@@ -37,6 +37,16 @@ export const deletePost = id => async dispatch => {
     await api.deletePost(id);
 
     dispatch({ type: DELETE, payload: id });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const likePost = id => async dispatch => {
+  try {
+    const { data } = await api.likePost(id);
+
+    dispatch({ type: LIKE, payload: data });
   } catch (error) {
     console.log(error.message);
   }
